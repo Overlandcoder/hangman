a3ec5b6ed32df9ada32970fbe917749d768360c3
Overlander <anuragnandal@hotmail.com>
2022-01-11 11:58:57 -0500

    Move draw_dashes to Board class

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,40 +1,39 @@
 class Game
+  attr_reader :word
 
   def initialize
     dictionary = File.open("5desk.txt", "r")
     words = File.readlines("5desk.txt", chomp: true)
-    random_word(words)
+    randomize(words)
   end
 
-
-  def random_word(words)
-  @word = ""
-
-    until !@word.empty?
-      if (words.sample.length >= 5 && words.sample.length <= 12)
-        @word = words.sample
+  def randomize(words)
+    @word = ""
+  
+      until !@word.empty?
+        if (words.sample.length >= 5 && words.sample.length <= 12)
+          @word = words.sample.downcase
+        end
       end
-    end
   end
 
   def play
     p @word
-    draw_dashes
-  end
-
-  def draw_dashes
-    dashes = ""
-    puts @word.length
-    @word.length.times do
-      dashes += "_"
-    end
-    puts dashes
+    board = Board.new
+    board.draw_dashes(@word)
   end
 end
 
 class Board
 
-
+  def draw_dashes(word)
+    dashes = ""
+    puts word.length
+    word.length.times do
+      dashes += "_"
+    end
+    puts dashes
+  end
 end
 
 game = Game.new
